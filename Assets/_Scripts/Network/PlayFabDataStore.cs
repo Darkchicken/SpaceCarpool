625815c95fb619f37226648ffbcd13bdb4c2fd7b
jedisongoku <beratbekce@gmail.com>
2016-03-19 16:19:01 -0700

    Data Store Updated

--- Assets/_Scripts/Network/PlayFabDataStore.cs
+++ Assets/_Scripts/Network/PlayFabDataStore.cs
@@ -10,17 +10,21 @@ public class PlayFabDataStore : MonoBehaviour
     public static string sessionTicket;
     public static string playFabId;
     public static string userName;
+    public static float playerLatitude;
+    public static float playerLongitude;
     public static string masterClientUserName;
     public static string masterClientPlayFabId;
     public static float masterClientLatitude;
     public static float masterClientLongitude;
-    
+
     /*
     Dictionary<string, string> customData = new Dictionary<string, string>(); //create a dictionary for the data
+    string locationString = playerLatitude + "#" + playerLongitude;
     customData.Add("Location", locationString); //locationString has to be this way: Latitude#Longtitude
     PlayFabApiCalls.UpdateUserLocation(customData); // then call this function and send the dictionary
 
     */
-    
-   
+
+
+
 }
