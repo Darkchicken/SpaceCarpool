face8c4f9eadbd9fc4eada8a8974776891a4aaef
jedisongoku <beratbekce@gmail.com>
2016-03-18 20:25:53 -0700

    Added a Scene for Testing Object Spawn

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public Transform spawnLocation;
+    public GameObject moon;
+    private Vector3 spawnLocationBoundry;
+
+	void Start ()
+    {
+        spawnLocationBoundry = spawnLocation.gameObject.GetComponent<Collider>().bounds.size;
+        Invoke("SpawnObject", 5f);
+        Invoke("SpawnObject", 10f);
+        Invoke("SpawnObject", 15f);
+        Invoke("SpawnObject", 20f);
+    }
+
+    void SpawnObject()
+    {
+        Instantiate(moon, spawnLocation.position, Quaternion.identity);
+    }
+}
---
+++ Assets/_Scripts/MoveObjects.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveObjects : MonoBehaviour {
+
+    
+    private Transform cameraTransform;
+    private float startTime;
+    private Rigidbody rb;
+    private Vector3 endPosition;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cameraTransform = Camera.main.transform;
+        startTime = Time.deltaTime;
+        endPosition = new Vector3(cameraTransform.position.x, cameraTransform.position.y, cameraTransform.position.z - 2000);
+    }
+
+	void FixedUpdate ()
+    {
+        transform.position = Vector3.Lerp(transform.position, endPosition, (Time.time - startTime) / 10000);
+	}
+	
+}
