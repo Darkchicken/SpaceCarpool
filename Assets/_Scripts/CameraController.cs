5df93a23f7af81142736743004cbef13ce9aea70
akobata <kobata.anne@gmail.com>
2016-03-20 16:59:55 -0700

    Added code for camera movement
    script added to folder, can test through TestCamera scene

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//attach this script to the camera to rotate the camera angle based on the accelerometer
+public class CameraController : MonoBehaviour {
+
+	void Update () {
+		//camera rotating side to side 
+		transform.Rotate (0, Input.acceleration.x, 0);
+
+		//Uncomment the following line for possible up/down and left/right movement
+		//transform.Rotate (-Input.acceleration.y / 2, Input.acceleration.x, 0);
+	}
+}
