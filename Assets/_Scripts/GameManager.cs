f9a5357aa9b1fb7b73791c476feba1be010e1b1c
Darkchicken <joshshucker@hotmail.com>
2016-03-23 11:04:26 -0700

    Only master spawns obstacles now

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -6,8 +6,8 @@ public class GameManager : MonoBehaviour {
     public static GameManager gameManager;
     public BoxCollider spawnArea;
     public BoxCollider destinationArea;
-    public GameObject asteroid;
-    public GameObject resource;
+    GameObject asteroid;
+    GameObject resource;
     //private Vector3 spawnLocationBoundry;
     private Vector3 colliderSize;
     private Vector3 spawnLocation;
@@ -16,7 +16,8 @@ public class GameManager : MonoBehaviour {
     {
         gameManager = this;
         colliderSize = spawnArea.GetComponent<BoxCollider>().size;
-        
+       
+
 
     }
 
@@ -28,22 +29,25 @@ public class GameManager : MonoBehaviour {
 
     IEnumerator SpawnObject()
     {
-        int randomNumber = Random.Range(0, 1000);
+        if (PhotonNetwork.isMasterClient)
+        {
+            int randomNumber = Random.Range(0, 1000);
 
-        if (randomNumber <= 500) //Spawn Asteroid
-        {
-            spawnLocation = new Vector3(Random.Range(spawnArea.transform.position.x - spawnArea.size.x / 2, spawnArea.transform.position.x + spawnArea.size.x / 2),
-            Random.Range(spawnArea.transform.position.y - spawnArea.size.y / 2, spawnArea.transform.position.y + spawnArea.size.y / 2), spawnArea.transform.position.z);
-            GameObject asteroidObject = Instantiate(asteroid, spawnLocation, Quaternion.identity) as GameObject;
-        }
-        else if(randomNumber <= 900) //Spawn Resource
-        {
-            spawnLocation = new Vector3(Random.Range(spawnArea.transform.position.x - spawnArea.size.x / 2, spawnArea.transform.position.x + spawnArea.size.x / 2),
-            Random.Range(spawnArea.transform.position.y - spawnArea.size.y / 2, spawnArea.transform.position.y + spawnArea.size.y / 2), spawnArea.transform.position.z);
-            GameObject resourceObject = Instantiate(resource, spawnLocation, Quaternion.identity) as GameObject;
-        }
-        else if (randomNumber <= 1000) // Spawn Fuel
-        {
+            if (randomNumber <= 500) //Spawn Asteroid
+            {
+                spawnLocation = new Vector3(Random.Range(spawnArea.transform.position.x - spawnArea.size.x / 2, spawnArea.transform.position.x + spawnArea.size.x / 2),
+                Random.Range(spawnArea.transform.position.y - spawnArea.size.y / 2, spawnArea.transform.position.y + spawnArea.size.y / 2), spawnArea.transform.position.z);
+                GameObject asteroidObject = PhotonNetwork.Instantiate("Asteroid", spawnLocation, Quaternion.identity, 0) as GameObject;
+            }
+            else if (randomNumber <= 900) //Spawn Resource
+            {
+                spawnLocation = new Vector3(Random.Range(spawnArea.transform.position.x - spawnArea.size.x / 2, spawnArea.transform.position.x + spawnArea.size.x / 2),
+                Random.Range(spawnArea.transform.position.y - spawnArea.size.y / 2, spawnArea.transform.position.y + spawnArea.size.y / 2), spawnArea.transform.position.z);
+                GameObject resourceObject = PhotonNetwork.Instantiate("Resource", spawnLocation, Quaternion.identity,0) as GameObject;
+            }
+            else if (randomNumber <= 1000) // Spawn Fuel
+            {
+            }
         }
 
         yield return new WaitForSeconds(Random.Range(0, 2));
