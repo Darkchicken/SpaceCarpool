492e3314765271e984f06e2e93f15274f2e2f729
Darkchicken <joshshucker@hotmail.com>
2016-04-21 13:17:24 -0700

    Added game over scene

---
+++ Assets/_Scripts/GameOverScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour {
+    //displays the reason for game over (returned to base, out of health, out of fuel)
+    public Text gameOverText;
+    //displays the final score for this session
+    public Text scoreText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        scoreText.text = "You gathered " + PlayFabDataStore.playerScore + " resources";
+	    if(PlayFabDataStore.shipHealth <= 0)
+        {
+            gameOverText.text = "Your ship was damaged";
+        }
+        else if(PlayFabDataStore.shipFuel <= 0)
+        {
+            gameOverText.text = "Your ship ran out of fuel";
+        }
+        else
+        {
+            gameOverText.text = "You ended the scavenging mission";
+        }
+	}
+	
+	public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("Login");
+    }
+}
