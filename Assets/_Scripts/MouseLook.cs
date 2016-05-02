8dcca1f3c688ef8113e38f8e0e19eaff1887ac16
Kyugetsu <anguyenh91@gmail.com>
2016-05-01 20:01:57 -0700

    Joystick Added
    Use Tab to switch between joystick mode and normal mode for now. It
    starts in Joystick mode

--- Assets/_Scripts/MouseLook.cs
+++ Assets/_Scripts/MouseLook.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class MouseLook : MonoBehaviour
 {
     public Camera playerCamera;
     int speed = 10;
+
+    bool JoyStick = true;
+    int JoyStickCounter = 1;
+    public float JoyStickSensitivity = 5f;
 
     public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
     public RotationAxes axes = RotationAxes.MouseXAndY;
@@ -24,55 +29,136 @@ public class MouseLook : MonoBehaviour
 
     void Update()
     {
-        if (axes == RotationAxes.MouseXAndY)
+        Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * sensitivityX;
+
+        if(Input.GetKeyDown("tab"))
         {
-            // Read the mouse input axis
-            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-
-            rotationX = ClampAngle(rotationX, minimumX, maximumX);
-            rotationY = ClampAngle(rotationY, minimumY, maximumY);
-
-            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
-            Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
-            //Debug.Log("X - Q :" + xQuaternion);
-            //Debug.Log("Y - Q :" + yQuaternion);
-
-            transform.localRotation = originalRotation * xQuaternion * yQuaternion;
-            /*if((xQuaternion.y >= -0.5f && xQuaternion.y <= 0.5f) && (yQuaternion.x >= -0.3f && yQuaternion.x <= 0.3f))
+            if(JoyStickCounter == 0)
             {
+                JoyStick = true;
+                JoyStickCounter = 1;
+            }
+
+            else
+            {
+                JoyStick = false;
+                JoyStickCounter = 0;
+            }
+            
+        }
+
+        if (JoyStick == true)
+        {
+
+            if (axes == RotationAxes.MouseXAndY)
+            {
+                // Read the mouse input axis
+                rotationX += CrossPlatformInputManager.GetAxis("Horizontal") * JoyStickSensitivity;
+                rotationY += CrossPlatformInputManager.GetAxis("Vertical") * JoyStickSensitivity;
+
+                rotationX = ClampAngle(rotationX, minimumX, maximumX);
+                rotationY = ClampAngle(rotationY, minimumY, maximumY);
+
+                Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+                Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
+                //Debug.Log("X - Q :" + xQuaternion);
+                //Debug.Log("Y - Q :" + yQuaternion);
+
                 transform.localRotation = originalRotation * xQuaternion * yQuaternion;
-            }*/
+                /*if((xQuaternion.y >= -0.5f && xQuaternion.y <= 0.5f) && (yQuaternion.x >= -0.3f && yQuaternion.x <= 0.3f))
+                {
+                    transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+                }*/
 
-        }
-        else if (axes == RotationAxes.MouseX)
-        {
-            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
-            rotationX = ClampAngle(rotationX, minimumX, maximumX);
+            }
 
-            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
-
-            transform.localRotation = originalRotation * xQuaternion;
-            /*if (xQuaternion.y >= -0.5f && xQuaternion.y <= 0.5f)
+            else if (axes == RotationAxes.MouseX)
             {
+                rotationX += CrossPlatformInputManager.GetAxis("Horizontal") * JoyStickSensitivity;
+                rotationX = ClampAngle(rotationX, minimumX, maximumX);
+
+                Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+
                 transform.localRotation = originalRotation * xQuaternion;
-            }*/
+                /*if (xQuaternion.y >= -0.5f && xQuaternion.y <= 0.5f)
+                {
+                    transform.localRotation = originalRotation * xQuaternion;
+                }*/
 
-        }
-        else
-        {
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = ClampAngle(rotationY, minimumY, maximumY);
-
-            Quaternion yQuaternion = Quaternion.AngleAxis(-rotationY, Vector3.right);
-
-            transform.localRotation = originalRotation * yQuaternion;
-            /*if (yQuaternion.x >= -0.3f && yQuaternion.x <= 0.3f)
+            }
+            else
             {
-                transform.localRotation = originalRotation * yQuaternion;
-            }*/
+                rotationY += CrossPlatformInputManager.GetAxis("Vertical") * JoyStickSensitivity;
+                rotationY = ClampAngle(rotationY, minimumY, maximumY);
 
-        }
+                Quaternion yQuaternion = Quaternion.AngleAxis(-rotationY, Vector3.right);
+
+                transform.localRotation = originalRotation * yQuaternion;
+                /*if (yQuaternion.x >= -0.3f && yQuaternion.x <= 0.3f)
+                {
+                    transform.localRotation = originalRotation * yQuaternion;
+                }*/
+
+            }
+        }   //end if statement joystick = true
+
+        /*--------------------------------------------------------------------------------------------------------------
+                GyroControls
+           -------------------------------------------------------------------------------------------------------------
+        */
+        if (JoyStick == false)
+        {
+
+            if (axes == RotationAxes.MouseXAndY)
+            {
+                // Read the mouse input axis
+                rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+                rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+                rotationX = ClampAngle(rotationX, minimumX, maximumX);
+                rotationY = ClampAngle(rotationY, minimumY, maximumY);
+
+                Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+                Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
+                //Debug.Log("X - Q :" + xQuaternion);
+                //Debug.Log("Y - Q :" + yQuaternion);
+
+                transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+                /*if((xQuaternion.y >= -0.5f && xQuaternion.y <= 0.5f) && (yQuaternion.x >= -0.3f && yQuaternion.x <= 0.3f))
+                {
+                    transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+                }*/
+
+            }
+            else if (axes == RotationAxes.MouseX)
+            {
+                rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+                rotationX = ClampAngle(rotationX, minimumX, maximumX);
+
+                Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+
+                transform.localRotation = originalRotation * xQuaternion;
+                /*if (xQuaternion.y >= -0.5f && xQuaternion.y <= 0.5f)
+                {
+                    transform.localRotation = originalRotation * xQuaternion;
+                }*/
+
+            }
+            else if (JoyStick == false)
+            {
+                rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+                rotationY = ClampAngle(rotationY, minimumY, maximumY);
+
+                Quaternion yQuaternion = Quaternion.AngleAxis(-rotationY, Vector3.right);
+
+                transform.localRotation = originalRotation * yQuaternion;
+                /*if (yQuaternion.x >= -0.3f && yQuaternion.x <= 0.3f)
+                {
+                    transform.localRotation = originalRotation * yQuaternion;
+                }*/
+
+            }
+        }  // end if statement joystick = false
         playerCamera.transform.rotation = Quaternion.Lerp(playerCamera.transform.rotation, transform.rotation, speed * Time.deltaTime);
     }
 
