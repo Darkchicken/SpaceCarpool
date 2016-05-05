3f1e0c8bc78bd955e0bc100e3730ed7182d3ea09
Darkchicken <joshshucker@hotmail.com>
2016-05-05 12:28:38 -0700

    Added title panel scene

---
+++ Assets/_Scripts/OpeningTransition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+/*
+Transition from the opening title panel to the login page
+*/
+public class OpeningTransition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        //jumps to login page in 3 seconds
+        Invoke("JumpToLogin", 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void JumpToLogin()
+    {
+        //add a fade?
+        SceneManager.LoadScene(1);
+    }
+}
